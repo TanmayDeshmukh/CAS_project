9642d4c1899d7becf69f0e203cd4b0f0e4560dc0
Tanmay <tanmay.dn@gmail.com>
2020-11-04 11:32:06 +0100

    updates

--- src/bugbot_urdf_description/setup.py
+++ src/bugbot_urdf_description/setup.py
@@ -15,7 +15,8 @@ setup(
         ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.py')),
-  	(os.path.join('share', package_name), glob('urdf/*'))
+  	(os.path.join('share', package_name), glob('urdf/*')),
+  	(os.path.join('share', package_name)+'/meshes', glob('meshes/*'))
     ],
     
     install_requires=['setuptools'],
