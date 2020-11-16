59536f9641f3f3927ee62edd7a80bc82f2c36521
PabloDavidAR <pablolala@gmail.com>
2020-11-16 09:36:36 +0100

    function without constraints added

--- src/MPC_controller/MPC.py
+++ src/MPC_controller/MPC.py
@@ -1,5 +1,5 @@
 from casadi import *
-
+import numpy as np
 
 #################################################################################
 # Design parameters
@@ -11,6 +11,10 @@ N = 3
 # 2. Number of state variables we take into account
 n_states = 3
 
+# 3. Q and R matrixes
+Q = np.matrix('1 0 0; 0 1 0; 0 0 1')
+R = np.matrix('1 0 0; 0 1 0; 0 0 1')
+
 #################################################################################
 # Parameters required previous to calculation of the desired equations
 #################################################################################
@@ -19,7 +23,12 @@ n_states = 3
 
 # 1. Read and store the input value from the required TOPIC.
 # 2. Estimate, via Kalman Filter, the new value of the state in that step (if the input value has already been done with the Kalman Filter, this step isn't required)
-# 3. Obtain the value of the desired N future states and actions from the required TOPIC.
+# 3. Obtain the value of the desired N future states and actions from the required TOPIC
+
+#We will need to change it to the read values
+
+u_ref = np.zeros((n_states,N))
+x_ref = np.zeros((n_states,N))
 
 ################################################################################  
 # Establish the variables that need to be optimised
@@ -31,9 +40,9 @@ state = [0 for i in range(0,N)]
 #print("Size of state: ", len(state))
 
 for i in range(0,N):
-	state_name = "x" + "% s" % i
-	state[i] = MX.sym(state_name, n_states, 1)
-	#print(type(state[i]))
+	state_name = "x_" + "% s" % i
+	state[i] = SX.sym(state_name, n_states, 1)
+	#print(state[i])
 
 # 2. Action on each step (u(k),...,u(k+N))
 
@@ -41,15 +50,33 @@ action = [0 for i in range(0,N)]
 #print("Size of action: ", len(action))
 
 for i in range(0,N):
-	action_name = "u" + "% s" % i
-	action[i] = MX.sym(action_name, n_states, 1)
-	#print(type(action[i]))
+	action_name = "u_" + "% s" % i
+	action[i] = SX.sym(action_name, n_states, 1)
+	#print(type(mtimes(mtimes(action[i].T,Q),action[i])))
 
 ################################################################################
 # Equations required for the calculation
 ################################################################################
 
-# 1. Establish the equation that requires to be optimised.
+# 1. Establish the function without constraints.
+func = SX(0)
+
+print((state[1] - x_ref[:,1].transpose()).size())
+print((((state[1] - x_ref[:,1])).T).size())
+
+for i in range(0,N-1):
+	func = func + mtimes(mtimes((state[i] - x_ref[:,i].transpose()).T,Q),(state[i] - x_ref[:,i]))
+	func = func + mtimes(mtimes((action[i] - u_ref[:,i].transpose()).T,R),(action[i] - u_ref[:,i]))
+
+func = (1/2)*func
+
+# Do we have to add the delta u term???
+
+# 2. Add constraints
+
+# 3. Generate the required function to optimize
+
+# f = Function('f',[state,action],func)
 
 ################################################################################
 # Calculation of the optimization problem
@@ -65,3 +92,9 @@ for i in range(0,N):
 
 # 1. Take the part of the solution that refers to the action of the next step amd store it in a variable.
 # 2. Send this value to the desired TOPIC. 
+
+################################################################################
+#IMPORTANT INFORMATION:
+################################################################################
+
+#The action vector has been considered of the same size has the state vector -> once we put everything seriously, we change it to the adequate size
