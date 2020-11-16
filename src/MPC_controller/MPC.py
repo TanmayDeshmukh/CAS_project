28cf4201a0e44dcaa4e0a24d0b212b6c0e0f2f4b
PabloDavidAR <pablolala@gmail.com>
2020-11-16 06:33:47 +0100

    First mods

--- src/MPC_controller/MPC.py
+++ src/MPC_controller/MPC.py
@@ -1,5 +1,16 @@
 from casadi import *
 
+
+#################################################################################
+# Design parameters
+#################################################################################
+
+# 1. Number of steps ahead we need to take into account
+N = 3
+
+# 2. Number of state variables we take into account
+n_states = 3
+
 #################################################################################
 # Parameters required previous to calculation of the desired equations
 #################################################################################
@@ -10,18 +21,35 @@ from casadi import *
 # 2. Estimate, via Kalman Filter, the new value of the state in that step (if the input value has already been done with the Kalman Filter, this step isn't required)
 # 3. Obtain the value of the desired N future states and actions from the required TOPIC.
 
-################################################################################
-# Equations required for the calculation
-################################################################################
-
-# 1. Establish the equation that requires to be optimised.
-
 ################################################################################  
 # Establish the variables that need to be optimised
 ################################################################################
 
 # 1. State variables on each step (x(k),...x(k+N))
+
+state = [0 for i in range(0,N)]
+print("Size of state: ", len(state))
+
+for i in range(0,N):
+	state_name = "x" + "% s" % i
+	state[i] = MX.sym(state_name, n_states, 1)
+	print(type(state[i]))
+
 # 2. Action on each step (u(k),...,u(k+N))
+
+action = [0 for i in range(0,N)]
+print("Size of action: ", len(action))
+
+for i in range(0,N):
+	action_name = "u" + "% s" % i
+	action[i] = MX.sym(action_name, n_states, 1)
+	print(type(action[i]))
+
+################################################################################
+# Equations required for the calculation
+################################################################################
+
+# 1. Establish the equation that requires to be optimised.
 
 ################################################################################
 # Calculation of the optimization problem
