feb509af78f3e9a487d10a8a83df87c371fc0fb3
PabloDavidAR <pablolala@gmail.com>
2020-11-18 11:43:11 +0100

    MPC controller with bound limits and constraints

--- src/MPC_controller/MPC_def.py
+++ src/MPC_controller/MPC_def.py
@@ -10,11 +10,11 @@ N = 3
 
 # 2. Number of state variables and actions we take into account
 n_state = 3
-n_action = 3
+n_action = 2
 
 # 3. Q and R matrixes
 Q = np.matrix('1 0 0; 0 1 0; 0 0 1')
-R = np.matrix('1 0 0; 0 1 0; 0 0 1')
+R = np.matrix([[1, 0],[0, 1]])
 
 #################################################################################
 # Parameters required previous to calculation of the desired equations
@@ -28,14 +28,15 @@ R = np.matrix('1 0 0; 0 1 0; 0 0 1')
 
 #We will need to change it to the read values
 
-u_ref = np.zeros((n_state,N))
+u_ref = np.zeros((n_action,N))
 x_ref = np.zeros((n_state,N))
 
 # 4. Define the A and B matrixes of the system for the next calculations, and the value of the different parameters that are contained in the A and B matrixes.
 
 dt = 0.1
 
-#A = np.matrix
+A = np.matrix([[1, 0, 0],[0, 1, 0],[0, 1, 0]])
+B = np.matrix([[1, 1],[1, 0],[0, 1]])
 
 ################################################################################  
 # Establish the variables that need to be optimised
@@ -54,23 +55,54 @@ action = SX.sym('u',n_action*N,1)
 ################################################################################
 
 # 1. Establish the function without constraints.
+
 func = SX(0)
 
-#print(mtimes((state[(2*n_state):(n_state*(2+1))] - x_ref[:,2].transpose()).T,Q))
-
-#print(type(mtimes(mtimes((state[(2*n_state):(n_state*(2+1))] - x_ref[:,2].transpose()).T,Q),(state[(2*n_state):(n_state(2+1))] - x_ref[:,2]))))
-
-for i in range(0,N-1):
+for i in range(0,N):
 	func = func + mtimes(mtimes((state[(i*n_state):(n_state*(i+1))] - x_ref[:,i].transpose()).T,Q),(state[(i*n_state):(n_state*(i+1))] - x_ref[:,i]))
-	func = func + mtimes(mtimes((action[(i*n_state):(n_state*(i+1))] - u_ref[:,i].transpose()).T,R),(action[(i*n_state):(n_state*(i+1))] - u_ref[:,i]))
+	#print(func)
+	func = func + mtimes(mtimes((action[(i*n_action):(n_action*(i+1))] - u_ref[:,i].transpose()).T,R),(action[(i*n_action):(n_action*(i+1))] - u_ref[:,i]))
+	#print(func)
 
 func = (1/2)*func
 
 # Do we have to add the delta u term???
 
-# 2. Add constraints
+# 2. Add constraints regarding the state-space relation
 
-# 3. Generate the required function to optimize
+token = SX.sym('t',3,1)
+
+for i in range(0,N-1):
+	token = mtimes(A,state[(i+1)*n_state-1:(i+2)*n_state-1]) + mtimes(B,action[(i+1)*n_action-1:(i+2)*n_action-1]) 
+	for i in range(0, token.size()[0]):
+		func += token[i]
+
+# 3. Add constraints regarding limits of the action values
+
+#lim_max = SX.sym('lim_max',action.size()[0] + state.size()[0],1)
+lim_max = DM(action.size()[0] + state.size()[0],1)
+
+v_limit = 10
+w_limit = 5
+
+x_max = 9999
+x_min = -9999
+
+#lim_min = SX.sym('lim_min',action.size()[0],1)
+lim_min = DM(action.size()[0] + state.size()[0],1)
+
+for i in range(0,action.size()[0] + state.size()[0]):
+	lim_max[i] = x_max
+	lim_min[i] = x_min
+
+for i in range(action.size()[0] + state.size()[0], lim_max.size()[0]):
+	if (((i - (action.size()[0] + state.size()[0])) % 2) == 0):
+		lim_max[i] = v_limit
+	elif (((i -(action.size()[0] + state.size()[0])) % 2) == 1):
+		lim_max[i] = w_limit
+	lim_min[i] = 0
+
+# 4. Generate the required function to optimize
 
 # We are using the func variable to optimize because the type required
 # by the solver is SX, not the function type.
@@ -96,7 +128,7 @@ for i in range((n_state + n_action) * N):
 
 # 3. Store the result of the calculation (can be useful to see how the robot deviates from the desired values)
 
-r = S(x0 = guess, lbg = 0, ubg = 0)
+r = S(x0 = guess, lbx = lim_min, ubx = lim_max, lbg = 0, ubg = 0)
 x_opt = r['x']
 print('x_opt: ', x_opt)
 
