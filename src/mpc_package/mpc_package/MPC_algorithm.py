edd933a95341942557961858ef01e63df3a23791
pablo <pablo.arandarod@gmail.com>
2020-11-25 20:00:04 +0100

    Some changes in MPC

--- src/mpc_package/mpc_package/MPC_algorithm.py
+++ src/mpc_package/mpc_package/MPC_algorithm.py
@@ -29,9 +29,12 @@ state_limits = np.array([-9999, 9999])
 u_ref = np.zeros((n_action, N))
 x_ref = np.zeros((n_state, N))
 
+dt = 0.1
+current_action = np.array([0,0,0])
+current_state = np.array([0,0,0])
 
 # Obtaining the output action to be applied
-output_action = MPC_controller(A = A, B = B, n_state = n_state, n_action = n_action, N = N, Q = Q, R = R, x_ref = x_ref, u_ref = u_ref, action_limit = action_limits, state_limit = state_limits)
+output_action = MPC_controller(A = A, B = B, n_state = n_state, n_action = n_action, N = N, Q = Q, R = R, x_ref = x_ref, u_ref = u_ref, action_limit = action_limits, state_limit = state_limits, current_action = current_action, current_state = current_state, dt = dt)
 
 # Apply action
 print(output_action)
